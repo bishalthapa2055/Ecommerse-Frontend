ead9143c37f13f19820dae4236c126e361dde274
Bishal Thapa <bishalthapab989@gmail.com>
2023-03-02 21:19:18 +0545

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import Container from "@mui/material/Container";
 import Header from "./components/header";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
+
 import Slider from "./components/slider/";
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Header />
       <Slider />
-    </ThemeProvider>
+    </>
   );
 }
 
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,6 +1,6 @@
 import React, { ImgHTMLAttributes } from "react";
 import myImage from "../../assets/img_1.png";
-import { styled } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 
 interface MyImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   sx?: {
@@ -30,7 +30,79 @@ const Slider: React.FC = () => {
         src={myImage}
 
         // {...imageProps}
-      />
+      ></img>
+      <Box
+        sx={{
+          // border: "1px solid black",
+          position: "absolute",
+          width: "492.42px",
+          height: "104px",
+          left: "770.03px ",
+          top: "296px",
+        }}
+      >
+        <Typography
+          sx={{
+            fontFamily: "Lato",
+            fontStyle: "normal",
+            fontWeight: 800,
+            fontSize: "48px",
+            lineHeight: "48px",
+            textAlign: "center",
+            textTransform: "uppercase",
+            color: "#FFFFFF",
+            // textShadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+          }}
+        >
+          stylist picks beat the heat
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-end",
+          padding: "18px  36px",
+          gap: "10px",
+          boxSizing: "border-box",
+
+          position: "absolute",
+          width: "175.16px",
+          height: "58px",
+          left: "920.66px",
+          top: "448px",
+
+          border: "5px solid red",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            textAlign: "center",
+            textTransform: "uppercase",
+            bordeRadius: "3px",
+            height: "10px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#ffffff",
+
+              width: "118px",
+              height: " 22px",
+
+              fontFamily: "Lato",
+              fontStyle: "normal",
+              fontWeight: 700,
+              fontSize: "21px",
+              lineHeight: "22px",
+            }}
+          >
+            SHOP NOW
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 };

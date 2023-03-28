a7d373a8e6248e516dd046b69239ff74cbf97e4f
Bishal Thapa <bishalthapab989@gmail.com>
2023-03-28 12:21:22 +0545

    added

---
+++ src/components/single-product-view/index.tsx
@@ -0,0 +1,15 @@
+import Container from "@mui/material/Container";
+import React from "react";
+import Headers from "../about-us/header/header";
+
+const Index = () => {
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Headers />
+      </Container>
+    </>
+  );
+};
+
+export default Index;

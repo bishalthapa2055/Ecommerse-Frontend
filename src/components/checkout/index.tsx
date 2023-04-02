e518956bae85c1dd6cdf378dbc0e88033935bf8b
Bishal Thapa <bishalthapab989@gmail.com>
2023-04-02 14:08:39 +0545

    added

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -1,12 +1,16 @@
 import { Container } from "@mui/material";
 import React from "react";
+import Content from "./contents";
 import Headers from "../about-us/header/header";
+import FooterForSinglePage from "../footer/footerForSinglePage";
 
 const Index = () => {
   return (
     <>
       <Container maxWidth="lg">
         <Headers />
+        <Content />
+        <FooterForSinglePage />
       </Container>
     </>
   );
